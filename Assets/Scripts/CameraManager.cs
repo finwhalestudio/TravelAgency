9ddf00a756990759d978694e662fcccdb40c5b6a
Sangmin Lee <56144743+sangminlee-looxidlabs@users.noreply.github.com>
2020-05-04 16:34:00 +0900

    infinity camera & json

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,60 +2,114 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+namespace FinwhaleStudio.TravelAgency
 {
-    public float startZoom = 30.0f;
-    public float zoomSpeed = 1.5f;
-    public float zoomMin = 10.0f;
-    public float zoomMax = 80.0f;
-
-    private Vector3 MouseDownPosition;
-
-    void Start()
+    public class CameraManager : MonoBehaviour
     {
-        Camera.main.orthographicSize = startZoom;
-    }
+        [Header("Zoom")]
+        public float startZoom = 30.0f;
+        public float zoomSpeed = 1.5f;
+        public float zoomMin = 10.0f;
+        public float zoomMax = 80.0f;
 
-    void Update()
-    {
-        // If mouse wheel scrolled vertically, apply zoom...
-        // TODO: Add pinch to zoom support (touch input)
-        if (Input.mouseScrollDelta.y != 0)
+        [Header("World")]
+        public Transform worldLeft;
+        public Transform groundLeft;
+        public Transform worldRight;
+        public Transform groundRight;
+        public Transform worldCenter;
+        private float worldWidth = 409.6f;
+
+        private Vector3 MouseDownPosition;
+
+        void Start()
         {
-            // Save location of mouse prior to zoom
-            var preZoomPosition = getWorldPoint(Input.mousePosition);
-
-            // Apply zoom (might want to multiply Input.mouseScrollDelta.y by some speed factor if you want faster/slower zooming
-            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - (Input.mouseScrollDelta.y * zoomSpeed), zoomMin, zoomMax);
-
-            // How much did mouse move when we zoomed?
-            var delta = getWorldPoint(Input.mousePosition) - preZoomPosition;
-
-            // Move the camera by the amount mouse moved, so that mouse is back in same position now.
-            Camera.main.transform.Translate(-delta.x, -delta.y, 0);
+            Camera.main.orthographicSize = startZoom;
         }
 
-        // When mouse is first pressed, just save location of mouse/finger.
-        if (Input.GetMouseButtonDown(0))
+        void Update()
         {
-            MouseDownPosition = getWorldPoint(Input.mousePosition);
+            // 카메라 위치에 따른 월드 위치 조정 (무한 카메라)
+            RaycastHit cameraHit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2.0f, Screen.height / 2.0f)), out cameraHit, 1 << LayerMask.NameToLayer("Ground"));
+            if (cameraHit.transform != null)
+            {
+                if (cameraHit.transform.Equals(groundLeft))
+                {
+                    // 센터보다 왼쪽에 있는데 worldLeft.pos.x보다 왼쪽으로 가는 순간 오른쪽 월드를 왼쪽으로 옮김.
+                    if (worldLeft.position.x < worldCenter.position.x)
+                    {
+                        if (Camera.main.transform.position.x < worldLeft.position.x)
+                        {
+                            worldRight.position = new Vector3(worldLeft.position.x - worldWidth, 0.0f, 0.0f);
+                            worldCenter.position = new Vector3(worldLeft.position.x - (worldWidth / 2.0f), 0.0f, 0.0f);
+                        }
+                    }
+                    // 왼쪽으로 옮겨져서 센터가 왼쪽에 있는데 worldLeft.pos.x보다 오른쪽으로 가는 순간 제자리로 돌림.
+                    else if (worldLeft.position.x > worldCenter.position.x)
+                    {
+                        if (Camera.main.transform.position.x > worldLeft.position.x)
+                        {
+                            worldRight.position = new Vector3(worldLeft.position.x + worldWidth, 0.0f, 0.0f);
+                            worldCenter.position = new Vector3(worldLeft.position.x + (worldWidth / 2.0f), 0.0f, 0.0f);
+                        }
+                    }
+                }
+                else
+                {
+                    // 센터보다 오른쪽에 있는데 worldRight.pos.x보다 오른쪽으로 가는 순간 왼쪽 월드를 오른쪽으로 옮김.
+                    if (worldRight.position.x > worldCenter.position.x )
+                    {
+                        if( Camera.main.transform.position.x > worldRight.position.x )
+                        {
+                            worldLeft.position = new Vector3(worldRight.position.x + worldWidth, 0.0f, 0.0f);
+                            worldCenter.position = new Vector3(worldRight.position.x + (worldWidth / 2.0f), 0.0f, 0.0f);
+                        }
+                    }
+                    // 오른쪽으로 옮겨져서 센터가 오른쪽에 있는데 worldRight.pos.x보다 왼쪽으로 가는 순간 제자리로 돌림.
+                    else if (worldRight.position.x < worldCenter.position.x)
+                    {
+                        if (Camera.main.transform.position.x < worldRight.position.x)
+                        {
+                            worldLeft.position = new Vector3(worldRight.position.x - worldWidth, 0.0f, 0.0f);
+                            worldCenter.position = new Vector3(worldRight.position.x - (worldWidth / 2.0f), 0.0f, 0.0f);
+                        }
+                    }
+                }
+            }
+
+            // 마우스 휠 사용했을 때.
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                // 마우스 휠 이동거리만큼 만큼 줌.
+                var preZoomPosition = getWorldPoint(Input.mousePosition);
+                Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - (Input.mouseScrollDelta.y * zoomSpeed), zoomMin, zoomMax);
+
+                // 줌되면서 마우스 클릭한 방향으로 이동.
+                var delta = getWorldPoint(Input.mousePosition) - preZoomPosition;
+                Camera.main.transform.Translate(-delta.x, -delta.y, 0);
+            }
+
+            // 마우스 왼쪽 버튼 클릭했을 때.
+            if (Input.GetMouseButtonDown(0))
+            {
+                MouseDownPosition = getWorldPoint(Input.mousePosition);
+            }
+
+            // 마우스로 드래그 중일때.
+            if (Input.GetMouseButton(0))
+            {
+                // 마우스 이동거리만큼 카메라 이동.
+                var delta = getWorldPoint(Input.mousePosition) - MouseDownPosition;
+                Camera.main.transform.Translate(-delta.x, -delta.y, 0);
+            }
         }
 
-        // While mouse button/finger is down...
-        if (Input.GetMouseButton(0))
+        private Vector3 getWorldPoint(Vector2 screenPoint)
         {
-            // Total distance finger/mouse has moved while button is down
-            var delta = getWorldPoint(Input.mousePosition) - MouseDownPosition;
-
-            // Adjust camera by distance moved, so mouse/finger stays at exact location (in world, since we are using getWorldPoint for everything).
-            Camera.main.transform.Translate(-delta.x, -delta.y, 0);
+            RaycastHit hit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(screenPoint), out hit);
+            return hit.point;
         }
-    }
-
-    private Vector3 getWorldPoint(Vector2 screenPoint)
-    {
-        RaycastHit hit;
-        Physics.Raycast(Camera.main.ScreenPointToRay(screenPoint), out hit);
-        return hit.point;
     }
 }
---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace FinwhaleStudio.TravelAgency
+{
+    public class Location
+    {
+        public float x;
+        public float y;
+
+        public Location(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public class Place
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public Location Pos { get; set; }
+    }
+    public class PlaceData
+    {
+        public List<Place> places;
+        public string version;
+    }
+
+    public class DataManager : MonoBehaviour
+    {
+        void Start()
+        {
+            PlaceData placeData = new PlaceData();
+            placeData.version = "1";
+            placeData.places = new List<Place>();
+            
+            Place newPlace = new Place { ID = "U018X2", Name = "Oxford", Pos = new Location(-2.75f, 124.09f) };
+            placeData.places.Add(newPlace);
+
+            string serializedObject = JsonConvert.SerializeObject(placeData);
+
+            string dataFolder = Directory.GetParent(Application.dataPath).FullName + @"\Data\";
+            if (!Directory.Exists(dataFolder))
+            {
+                Directory.CreateDirectory(dataFolder);
+            }
+            string locationFilePath = dataFolder + @"places.json";
+            File.WriteAllText(locationFilePath, serializedObject);
+
+            //var player = JsonConvert.DeserializeObject<DBPlayer>(serializedObject);
+        }
+    }
+}
